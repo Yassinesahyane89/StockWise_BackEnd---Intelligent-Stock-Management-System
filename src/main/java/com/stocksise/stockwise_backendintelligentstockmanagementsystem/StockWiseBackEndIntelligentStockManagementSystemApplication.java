b9687c7e7123b001ffa6c8c347a0854a7d347b1f
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2024-03-12 10:30:02 +0000

    Initial commit

---
+++ src/main/java/com/stocksise/stockwise_backendintelligentstockmanagementsystem/StockWiseBackEndIntelligentStockManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.stocksise.stockwise_backendintelligentstockmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockWiseBackEndIntelligentStockManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockWiseBackEndIntelligentStockManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stocksise/stockwise_backendintelligentstockmanagementsystem/StockWiseBackEndIntelligentStockManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stocksise.stockwise_backendintelligentstockmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockWiseBackEndIntelligentStockManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
