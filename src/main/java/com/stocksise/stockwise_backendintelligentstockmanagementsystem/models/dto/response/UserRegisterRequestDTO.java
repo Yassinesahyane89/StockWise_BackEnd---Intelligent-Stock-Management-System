ec3f13a9c73080b2f9e7fbf1263e79475688d9ac
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2024-03-13 10:00:41 +0000

    Implemented UserRestController for handling user-related REST endpoints
    This commit:
    - Created UserRestController class to serve as the controller for user-related REST endpoints.
    - Implemented endpoints for user retrieval, creation, update, password change, and deletion.
    - Ensured proper mapping of HTTP methods to corresponding controller methods.
    - Utilized UserService for handling business logic related to user operations.
    - Applied appropriate request mappings and response handling for RESTful interaction.

---
+++ src/main/java/com/stocksise/stockwise_backendintelligentstockmanagementsystem/models/dto/response/UserRegisterRequestDTO.java
@@ -0,0 +1,48 @@
+package com.stocksise.stockwise_backendintelligentstockmanagementsystem.models.dto.response;
+
+import com.stocksise.stockwise_backendintelligentstockmanagementsystem.models.entities.User;
+import com.stocksise.stockwise_backendintelligentstockmanagementsystem.models.enums.userStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class UserRegisterRequestDTO {
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @NotBlank(message = "Email is required")
+    @Pattern(regexp = "^(.+)@(.+)$", message = "Invalid email pattern")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$", message = "Password must contain at least 8 characters, including one letter and one number")
+    private String password;
+
+    @NotBlank(message = "Confirm password is required")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$", message = "Password must contain at least 8 characters, including one letter and one number")
+    // same as password
+    private String confirmPassword;
+
+    @NotBlank(message = "Status is required")
+    private userStatus status;
+
+    public User toUser() {
+        return User.builder()
+                .firstName(this.firstName)
+                .lastName(this.lastName)
+                .email(this.email)
+                .password(this.password)
+                .status(this.status)
+                .build();
+    }
+}
