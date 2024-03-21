327ad5149b27c92c0ad9ef190e546b2dd8d3eb7c
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2024-03-21 22:09:34 +0000

    - Implemented BrandService interface, defining all method signatures required for brand management.

---
+++ src/main/java/com/stocksise/stockwise_backendintelligentstockmanagementsystem/services/BrandService.java
@@ -0,0 +1,13 @@
+package com.stocksise.stockwise_backendintelligentstockmanagementsystem.services;
+
+import com.stocksise.stockwise_backendintelligentstockmanagementsystem.models.entities.Brand;
+
+import java.util.List;
+
+public interface BrandService {
+    List<Brand> getAllBrands();
+    Brand getBrandById(Long id);
+    Brand saveBrand(Brand brand);
+    Brand updateBrand(Long id, Brand brand);
+    void deleteBrand(Long id);
+}
