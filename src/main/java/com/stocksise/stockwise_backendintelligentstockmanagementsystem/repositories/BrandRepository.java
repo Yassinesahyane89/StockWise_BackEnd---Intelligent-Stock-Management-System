722ba18c5f055843475dc157acabc274e280ec2d
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2024-03-21 22:08:47 +0000

    - Created BrandRepository to handle CRUD operations for Brand entities in the database.

---
+++ src/main/java/com/stocksise/stockwise_backendintelligentstockmanagementsystem/repositories/BrandRepository.java
@@ -0,0 +1,14 @@
+package com.stocksise.stockwise_backendintelligentstockmanagementsystem.repositories;
+
+import com.stocksise.stockwise_backendintelligentstockmanagementsystem.models.entities.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface BrandRepository extends JpaRepository<Brand, Long> {
+    // find brand by name
+    Optional<Brand> findByName(String name);
+
+    // find brand by email
+    Optional<Brand> findByEmail(String email);
+}
